e38af2ddb4b83d8218e1d01e31f4ac56884e191c
adarshms <adarsh@accubits.com>
2020-08-04 17:02:36 +0530

    Add test images

--- brightness_test.jl
+++ brightness_test.jl
@@ -3,6 +3,8 @@ using BenchmarkTools
 using PyCall
 
 
+# Julia implemention
+
 function im2arr(im_path)
     im = load(im_path)
     im_float_arr = permutedims(channelview(im), (2, 3, 1))
@@ -21,11 +23,15 @@ function color_balance(im_arr, r, g, b)
     return im_arr_cpy
 end
 
-function adjust_brightness(im_path, factor)
-    im_arr = im2arr(im_path)
-    color_balance(im_arr, factor, factor, factor)
+function adjust_brightness(im_paths, factor)
+    for im_path in im_paths
+        im_arr = im2arr(im_path)
+        color_balance(im_arr, factor, factor, factor)
+    end
 end
 
+
+# Python numpy implemention
 
 py"""
 from PIL import Image
@@ -55,13 +61,17 @@ def color_balance(im_arr, r, g, b):
     return im_arr_cpy
 
 
-def adjust_brightness(im_path, factor):
-    im_arr = im2arr(im_path)
-    im_arr = color_balance(im_arr, factor, factor, factor)
+def adjust_brightness(im_paths, factor):
+    for im_path in im_paths:
+        im_arr = im2arr(im_path)
+        im_arr = color_balance(im_arr, factor, factor, factor)
 
 """
 
 adj_br_np_py = py"adjust_brightness"
+
+
+# Python opencv implemention
 
 py"""
 import cv2
@@ -69,26 +79,30 @@ import numpy as np
 import time
 
 
-def adjust_brightness_cv(im_path, factor):
-    im_arr = cv2.imread(im_path)
-    im_arr = cv2.addWeighted(im_arr, 1, np.zeros(im_arr.shape, im_arr.dtype), 0, factor)
+def adjust_brightness_cv(im_paths, factor):
+    for im_path in im_paths:
+        im_arr = cv2.imread(im_path)
+        im_arr = cv2.addWeighted(im_arr, 1, np.zeros(im_arr.shape, im_arr.dtype), 0, factor)
 """
 
 adj_br_cv_py = py"adjust_brightness_cv"
 
 
-file_path = "/home/necromancer/Pictures/searchyfai_dasboard.png"
+# Run benchmark
+
+file_paths = ["images/001.jpg", "images/002.jpg", "images/003.jpg", "images/004.jpg"]
 
 benchmark_dict = Dict()
 
-j_bench = @benchmark adjust_brightness(file_path, 50)
+j_bench = @benchmark adjust_brightness(file_paths, 50)
 benchmark_dict["Julia"] = minimum(j_bench.times) / 1e6
 
-py_np_bench = @benchmark $adj_br_np_py(file_path, 50)
+py_np_bench = @benchmark $adj_br_np_py(file_paths, 50)
 benchmark_dict["Python Numpy"] = minimum(py_np_bench.times) / 1e6
 
-py_cv_bench = @benchmark $adj_br_cv_py(file_path, 50)
+py_cv_bench = @benchmark $adj_br_cv_py(file_paths, 50)
 benchmark_dict["Python Opencv"] = minimum(py_cv_bench.times) / 1e6
 
 
+# unit in milliseconds
 println(benchmark_dict)
